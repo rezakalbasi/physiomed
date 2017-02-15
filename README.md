04f676282c646eb9ea1f00b582b6fa33a9934dcb
Andrey <biokill@ymail.com>
2017-02-15 22:04:48 +1300

    play with a simple vuejs project

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
+# physiomed
 
-We'll use the Meteor framework at the moment though it's not the best option for a simple single page application.
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app">
+    <select>
+      <option v-for="person in people">
+        {{ person.name }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      people: [{
+        name: "Chang Fujita"
+      }, {
+        name: "Eskandari"
+      }, {
+        name: "Mackenzie"
+      }, {
+        name: "Moss"
+      }, {
+        name: "Thomas"
+      }, {
+        name: "Weinstein"
+      }]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
