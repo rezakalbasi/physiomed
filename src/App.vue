9abbaced443236b69e3e7b9813a5aa57d3355af9
rezakalbasi <r.kalbasi@auckland.ac.nz>
2017-04-04 22:27:08 +1200

    PhysioMedApp by Reza 04042017

--- src/App.vue
+++ src/App.vue
@@ -28,38 +28,88 @@
           </md-card>
         </md-layout>
 
+
+
+
         <md-layout>
-          <md-input-container>
-            <label>CP Concepts</label>
-            <md-input v-model="cp"></md-input>
-          </md-input-container>
-
-          <md-card style="width: 100%">
-            <md-card-header>
-              <div class="md-title">Computational Physiology Concept</div>
+          <md-card style="width: 120%">
+             
+             
+             <div id="app-6">
+                  <!--p>{{ message }}</p-->
+                  <input v-model="message">
+             </div>
+          
+          
+          
+          <div class="grid_10 omega" style="margin-left:30px">
+      <h3>Select a Computational Physiology Concept</h3>
+                <form id="local-search" name="local-search" action="http://www.ebi.ac.uk/ols/search" method="get">
+                    <fieldset><div class="left">
+                        <label>
+                            <input v-model="message2" style="font-weight: normal" size="45" type="text" name="q" data-olswidget="select" data-olsontology="" data-selectpath="http://www.ebi.ac.uk/ols/" olstype="" id="local-searchbox" placeholder="Enter the term you are looking for" class="ac_input">
+                            </input>
+                        </label>
+                    </div>
+                        <div class="right">
+                            <input type="submit" value="Search" class="submit">
+                            </input>
+                        </div>
+                    </fieldset>
+                </form>
+                <br>
+                <i>Note: This first sample demonstrates the 'select' functionality of App, just enter a term (e.g. cancer)</i>
+</div>
+            <md-card-header>              
+              <md-layout md-align="center end">
+      <div><h2>Computational Physiology Conecept Ontology</h2></div>
+      <div id="term-tree"    style="height:300px; width:400px; border:1px solid white;"></div>
+              </md-layout>
             </md-card-header>
-
-            <md-card-content>
-              {{ cp }}
-            </md-card-content>
           </md-card>
         </md-layout>
       </md-layout>
 
+
+
+
+
+
+
+
+
+<!--22222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222  -->
+<!--22222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222  -->
+
+
+
       <md-layout md-gutter v-if="pageNumber === 2">
+
+<!--div id="local-search1">
+  <p>{{ message2 }}</p>
+  http://purl.obolibrary.org/obo/CHEBI_17996
+</div-->
+
         <md-layout md-align="center end">
           <md-list>
-            <md-list-item v-for="response in responseBody">
+            <md-list-item v-for="(response,u) in a">
               <span style="margin-right: 20px">
-                {{ response[3] }}
-                <a v-bind:href="response[2]" target="_blank">[{{ response[2] }}]</a>
+
+               {{ a[u] }}   
+                <a  v-bind:href="b[u]" target="_blank">[{{ b[u] }}]</a>
+              
               </span>
               <md-checkbox class="md-primary" v-model="checkbox">
               </md-checkbox>
             </md-list-item>
           </md-list>
-        </md-layout>
+        </md-layout>    
       </md-layout>
+
+
+<!--88888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888  -->
+<!--88888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888  -->
+
 
       <md-layout md-gutter v-if="pageNumber === 3">
         <md-layout md-align="center">
@@ -72,14 +122,18 @@
             </md-table-header>
 
             <md-table-body>
-              <md-table-row v-for="response in responseBody">
-                <md-table-cell>{{ response[3] }}</md-table-cell>
+              <md-table-row v-for="(response,u) in a">
+                <md-table-cell>{{ a[u] }}</md-table-cell>
                 <md-table-cell></md-table-cell>
               </md-table-row>
             </md-table-body>
           </md-table>
         </md-layout>
       </md-layout>
+
+
+<!--88888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888  -->
+<!--88888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888  -->
 
       <md-layout md-gutter v-if="pageNumber === 4">
         <md-layout>
@@ -186,6 +240,9 @@
           name: "weinstein_1995-semgen.cellml"
         }],
         responseBody: [],
+        a:[],
+        b:[],
+        c:[],
         cellml: null,
         cp: null,
         placeholder: this.cellml ? 'Call API for ' + this.cellml : 'No one is selected yet',
@@ -198,9 +255,50 @@
         formData.append('cellml', `https://models.physiomeproject.org/workspace/267/rawfile/240aec39cbe4a481af115b02aac83af1e87acf2e/semgen-annotation/${cellml}`);
         this.$http.post('http://127.0.0.1:5000/pmr', formData).then(response => {
           this.responseBody = response.body;
+var aaa="http://purl.obolibrary.org/obo/CHEBI_17996";
+          var n;
+          n= this.responseBody.length;
+          var i;
+          var j;
+          for(i = 0; i < n; i++){
+                      this.a.push(response.body[i][3]);
+                                            
+                                }
+                   this.a.sort();
+                   for(j = 0; j < n; j++) {            
+                    if (this.a[j]==this.a[j+1]) {
+                      delete this.a[j];
+                                                 }
+                    
+                                          }
+                   this.a.sort();
+                   this.a = this.a.filter(Boolean);
+           
+           
+           
+           var r;
+           var q;
+           var z;
+           z= this.a.length;
+           for(r = 0; r < z; r++){
+             for(q = 0; q < n; q++){
+                  if (this.a[r]==response.body[q][3])  {this.b.push(response.body[q][2]);
+                  break;}
+                                    }
+                                  }
+
+    /*      
+          var t;
+          for(t = 0; t < z; t++){
+                      this.c[t] =  this.a[t]+" ("+ this.b[t]+")";
+                      //this.c[t] = this.a[t]+"("+ this.b[t].link() +")";
+                      
+                               }
+      */    
         });
       }
     },
+    
     methods: {
       // callToAPI() {
       // const select = document.querySelector(".js-select");
